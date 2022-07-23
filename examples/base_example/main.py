8d5e94f49d74ca82989679cf12c6f4e9ae99d8b4
Neizvestnyj <40869738+Neizvestnyj@users.noreply.github.com>
2022-07-23 18:09:55 +0300

    added print

--- examples/base_example/main.py
+++ examples/base_example/main.py
@@ -6,4 +6,4 @@ files_folder = os.path.join(pathlib.Path(__file__).parent.resolve().parent, 'fil
 stl = os.path.join(files_folder, 'cube.stl')
 obj = os.path.join(files_folder, 'cube.obj')
 a = Stl2Obj()
-a.convert(stl, obj)
+print(a.convert(stl, obj))
