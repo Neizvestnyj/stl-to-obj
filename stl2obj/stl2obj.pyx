d1dc9247c97b8dd2b062bd77aa678a405be7b1e2
Neizvestnyj <40869738+Neizvestnyj@users.noreply.github.com>
2022-07-24 14:23:54 +0300

    check if src and dst does not exists

--- stl2obj/stl2obj.pyx
+++ stl2obj/stl2obj.pyx
@@ -2,6 +2,8 @@
 # distutils: language=c++
 
 from libcpp.string cimport string
+from os.path import exists
+from pathlib import Path
 
 # Redeclare the .h in cython space
 cdef extern from "src/_stl2obj.h":
@@ -13,7 +15,15 @@ use normal def if your cython code don't need to do anything else but wrapping i
 '''
 
 cdef class Stl2Obj:
-    def convert(self, src, dst):
+    def convert(self, src: str, dst: str):
+        if not exists(src):
+            raise FileNotFoundError(f'{src} does not exists')
+
+        p = Path(dst)
+
+        if not exists(p.parent):
+            raise OSError(f'{p.parent} directory does not exist')
+
         cdef cpp_src = <string>src.encode('utf-8')
         cdef cpp_dst = <string>dst.encode('utf-8')
 
