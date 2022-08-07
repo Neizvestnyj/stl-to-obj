3053ffc6bc7ccf207455a5618a89a96be3a13580
Neizvestnyj <40869738+Neizvestnyj@users.noreply.github.com>
2022-08-07 16:09:11 +0300

    update stl files folder

--- examples/base_example/main.py
+++ examples/base_example/main.py
@@ -2,7 +2,7 @@ from stl2obj import Stl2Obj
 import os
 import pathlib
 
-current_folder = os.path.join(pathlib.Path(__file__).parent.resolve())
+current_folder = os.path.join(pathlib.Path(__file__).parents[2].resolve(), 'files')
 
 stl = os.path.join(current_folder, 'cube.stl')
 obj = os.path.join(current_folder, 'cube.obj')
